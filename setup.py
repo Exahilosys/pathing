545653bb125895ccbab51148d0598dd9e643c38c
Exahilosys <exahilosys@gmail.com>
2019-05-10 23:20:48 +0100

    switched to module

--- setup.py
+++ setup.py
@@ -6,27 +6,26 @@ with open('README.md') as file:
 
 name = 'pathing'
 
-version = '0.1.1'
+version = '0.1.2'
 
 author = 'Exahilosys'
 
 url = f'https://github.com/{author}/{name}'
 
-download_url = f'{url}/archive/v{version}.tar.gz'
-
 setuptools.setup(
     name = name,
     version = version,
     author = author,
-    author_email = 'exahilosys@gmail.com',
     url = url,
-    download_url = download_url,
     packages = setuptools.find_packages(),
     license = 'MIT',
     description = 'Mapping keys path to value derivation.',
     long_description = readme,
     long_description_content_type = 'text/markdown',
     include_package_data = True,
+    py_modules = [
+        name
+    ],
     classifiers = [
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
